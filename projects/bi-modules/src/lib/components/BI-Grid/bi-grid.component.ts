432c3ef0a678c644d5881632f6afed30f6869234
Basel ElSawy <basel.elsawy@bi-technologies.net>
2023-05-28 10:00:38 +0300

    Create Array to CreatedItem and UpdatedItem

--- projects/bi-modules/src/lib/components/BI-Grid/bi-grid.component.ts
+++ projects/bi-modules/src/lib/components/BI-Grid/bi-grid.component.ts
@@ -1,12 +1,12 @@
-import { AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { CellClickEvent, CellCloseEvent, CreateFormGroupArgs, GridComponent, GridItem, PageChangeEvent } from '@progress/kendo-angular-grid';
 import { State, toODataString } from "@progress/kendo-data-query";
-import { IColumns } from 'bi-interfaces/lib/interfaces/IColumns.interface';
 import { AlertService } from '@full-fledged/alerts';
-import { Observable } from 'rxjs';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { IColumns } from 'bi-interfaces/lib/interfaces/IColumns.interface';
 import { IDataService } from 'bi-interfaces/lib/interfaces/IDataService';
 import { IGrid } from 'bi-interfaces/lib/interfaces/IGrid';
+import { BehaviorSubject, Observable, Subject, of, take } from 'rxjs';
 @Component({
 	selector: 'BI-Grid',
 	templateUrl: './bi-grid.component.html',
@@ -20,6 +20,9 @@ export class BIGridComponent implements IGrid, OnInit {
 	@Input() refGrid!: string;
 	@Output() CellClick = new EventEmitter<CellClickEvent>();
 	@ViewChild("Grid") Mygrid!: GridComponent;
+	StopSave!: Subject<boolean>;
+	CreatedItemArray: Array<Object> = [];
+	UpdatedItemArray: Array<Object> = [];
 	form: any = {};
 	CurrentSelectRow!: FormGroup;
 	GridData!: any;
@@ -31,7 +34,7 @@ export class BIGridComponent implements IGrid, OnInit {
 
 	constructor(
 		private formBuilder: FormBuilder,
-		private alertService: AlertService, 
+		private alertService: AlertService,
 		private cd: ChangeDetectorRef
 	) { }
 
@@ -40,13 +43,15 @@ export class BIGridComponent implements IGrid, OnInit {
 		this.DataService.read(`$skip=${this.state.skip}&$top=10&$count=true`);
 		this.handleFormGroup();
 		this.createFormGroup = this.createFormGroup.bind(this);
+		this.StopSave = new Subject<boolean>();
+		this.StopSave.next(true);
 	}
 
 	handleFormGroup() {
 		this.Columns.forEach(res => this.form[res.Name] = [{ value: res.DefaultValue, disabled: !res.IsEditable }, res.Validators]);
 		if (!this.CurrentSelectRow?.controls) this.CurrentSelectRow = this.formBuilder.group(this.form);
 		this.CurrentSelectRow.valueChanges.subscribe(res => {
-			if(this.dataItem) {
+			if (this.dataItem) {
 				Object.assign(this.dataItem, res);
 			}
 		})
@@ -66,7 +71,7 @@ export class BIGridComponent implements IGrid, OnInit {
 		});
 	}
 
-	BeforeAction() {
+	BeforeAction(): void {
 	}
 
 	public pageChange(event: PageChangeEvent): void {
@@ -77,13 +82,13 @@ export class BIGridComponent implements IGrid, OnInit {
 
 	cellCloseHandler(args: CellCloseEvent) {
 		const { formGroup, dataItem } = args;
-
 		if (!formGroup.valid) {
 			// prevent closing the edited cell if there are invalid values.
 			args.preventDefault();
 		}
 		this.assignValues(dataItem, formGroup.value);
 		this.CurrentSelectRow.patchValue(formGroup.value);
+		if (formGroup.dirty && !isNaN(this.rowIndex)) this.UpdatedItemArray.push({...formGroup.value});
 	}
 
 	public assignValues(target: any, source: any): void {
@@ -95,7 +100,7 @@ export class BIGridComponent implements IGrid, OnInit {
 	}
 
 	AddRow() {
-		this.CurrentSelectRow?.reset()
+		this.Cancel();
 		this.Mygrid.addRow(this.createFormGroup(this.newForm));
 	}
 
@@ -129,34 +134,39 @@ export class BIGridComponent implements IGrid, OnInit {
 		this.GetGridData();
 		this.Mygrid.closeRow(this.rowIndex);
 		this.CurrentSelectRow.reset();
-		this.dataItem = null
+		this.dataItem = null;
 	}
 
-	Save() {
-		this.BeforeAction();
-		if (isNaN(this.rowIndex)) {
-			// Save Create
-			if (this.CurrentSelectRow?.valid) {
-				this.DataService.add(this.CurrentSelectRow.getRawValue()).subscribe((res: any) => {
-					this.data['data'].push(this.CurrentSelectRow.getRawValue());
-					this.Mygrid.closeRow();
-					this.GetGridData();
-					this.handleFormGroup();
-					this.alertService.success("Saved Successfully");
-				});
-			};
-		} else {
-			// Save Update
-			if (this.CurrentSelectRow?.valid) {
-				this.DataService.edit(this.CurrentSelectRow.getRawValue(), this.CurrentSelectRow.getRawValue()[this.Key]).subscribe((res: any) => {
-					this.Mygrid.closeRow(this.rowIndex);
-					this.DataService.read(`$skip=${this.state.skip}&$top=10&$count=true`);
-					this.GetGridData();
-					this.handleFormGroup();
-					this.alertService.success("Saved Successfully");
-				});
-			};
-		};
+	async Save() {
+		if(!this.dataItem) this.CreatedItemArray.push({...this.createFormGroup(this.newForm).value});
+		await this.BeforeAction();
+		this.StopSave?.pipe(take(1)).subscribe((res: any) => {
+			if (res) {
+				if (isNaN(this.rowIndex)) {
+					// Save Create
+					if (this.CurrentSelectRow?.valid) {
+						this.DataService.add(this.CurrentSelectRow.getRawValue()).subscribe((res: any) => {
+							this.data['data'].push(this.CurrentSelectRow.getRawValue());
+							this.Mygrid.closeRow();
+							this.GetGridData();
+							this.handleFormGroup();
+							this.alertService.success("Saved Successfully");
+						});
+					};
+				} else {
+					// Save Update
+					if (this.CurrentSelectRow?.valid) {
+						this.DataService.edit(this.CurrentSelectRow.getRawValue(), this.CurrentSelectRow.getRawValue()[this.Key]).subscribe((res: any) => {
+							this.Mygrid.closeRow(this.rowIndex);
+							this.DataService.read(`$skip=${this.state.skip}&$top=10&$count=true`);
+							this.GetGridData();
+							this.handleFormGroup();
+							this.alertService.success("Saved Successfully");
+						});
+					};
+				};
+			}
+		});
 	}
 
 }
